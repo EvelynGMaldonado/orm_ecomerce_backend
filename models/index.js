76bc1c30ca72dfbbdd5f3934314cac733f783e35
Evelyn G <evelyn.gmaldonado@gmail.com>
2021-10-28 22:11:37 -0700

    server, config, belongs to, belongs to many, has many are done

--- models/index.js
+++ models/index.js
@@ -3,10 +3,17 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+// const User = require("./User");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: category_id,
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
