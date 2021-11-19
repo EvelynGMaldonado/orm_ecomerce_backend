41ec43b6471c444c070c12f4ec6afba101ff613f
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-11-18 17:53:31 -0800

    finishing readme and interacting with product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -54,20 +54,26 @@ router.get('/:id', (req, res) => {
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
   */
-  Product.create({
-    product_name: "Basketball",
-    price: 200.00,
-    stock: 3,
-    category_id: [1, 2, 3, 4],
-    // tagIds: [1, 2, 3, 4] 
-  }
+  // Product.create({
+  //   product_name: "Basketball",
+  //   price: 200.00,
+  //   stock: 3,
+  //   category_id: [1, 2, 3, 4],
+  //   tagIds: [1, 2, 3, 4] 
+  // }
+//   try {
+//     const productData = await Product.create({
+//     product_name: req.body.product_name,
+//     price:req.body.price,
+//     stock: req.body.stock,
+//     tag_id: req.body.tag_id,
+//   });
+//   res.status(200).json(productData);
+// } catch (err) {
+//   res.status(400).json(err);
+// }
+Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -86,7 +92,7 @@ router.post('/', (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
-    }));
+    });
 });
 
 // update product
